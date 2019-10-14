6a49c2ccc8ad6c67345e8e5d039f88f46d13093d
Naoya Ozawa <n.ozawa@cns.s.u-tokyo.ac.jp>
2019-10-14 11:04:58 +0900

    Fr production curve calculator

--- decaychain.cpp
+++ decaychain.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TMultiGraph.h>
+#include <TGraph.h>
+#include <TMath.h>
+#include <TRint.h>
+using namespace std;
+
 double parent(double *x,double *par){
 
 	double flux = par[0]; // particles per second as incoming beam
@@ -30,7 +39,20 @@ double daughter(double *x,double *par){
 	return coef_daughter*exp_daughter + coef_alphaParent*exp_alphaParent + coef_betaParent*exp_betaParent;
 }
 
+double ni_ratio(double T, double E_wf, double E_ip, double sw){
+	double k = 8.61733 * TMath::Power(10.,-5); // eV/K
+	return sw * TMath::Exp( (E_wf - E_ip) / (k*T) );
+}
+
+double ionization(double T, double E_wf, double E_ip, double sw){
+	return ni_ratio(T,E_wf,E_ip,sw) / (1.0 + ni_ratio(T,E_wf,E_ip,sw));
+}
+
 int main (int argc, char** argv){
+
+	TRint rootapp("app",&argc,argv);
+
+	TCanvas *c1 = new TCanvas();
 
 	double seconds = 1.0;
 	double minutes = 60.0;
@@ -38,44 +60,115 @@ int main (int argc, char** argv){
 	double days = 24.0 * hours;
 	double years = 365.0 * days;
 
-	double R_211fr = 0.19;
+	double secondaryFlux = 1.0*TMath::Power(10.,7); // Particles per second
+	double T = 800.0; // K
+	double timelimit = 10.0*minutes;
 
+	double E_wf_Mo = 4.6; // eV
+	double E_wf_Au = 5.1; // eV
+
+	double E_ip_Fr = 4.07; // eV
+
+	double R_210fr = 0.632;
+	double br_210fr = 0.71;
+	double t_210fr = 3.18*minutes;
+	double e_210fr = 6.5; // MeV
+
+	TF1 *N_210fr = new TF1("N_210Fr",parent,0.,timelimit,2);
+	N_210fr->SetParameters(secondaryFlux*R_210fr*ionization(T,E_wf_Au,E_ip_Fr,0.5),t_210fr);
+	TGraph *g_210fr = new TGraph(N_210fr);
+	g_210fr->SetMarkerColor(kRed);
+	g_210fr->SetLineColor(kRed);
+
+	double R_211fr = 0.034;
 	double br_211fr = 0.80;
-	double brerr_211fr = 0.20;
 	double t_211fr = 3.10*minutes;
-	double terr_211fr = 0.02*minutes;
+	double e_211fr = 6.7; // MeV
 
-	double br_211rn = 0.274;
-	double brerr_211rn = 0.017;
-	double t_211rn = 14.6*hours;
-	double terr_211rn = 0.2*hours;
+	TF1 *N_211fr = new TF1("N_211Fr",parent,0.,timelimit,2);
+	N_211fr->SetParameters(secondaryFlux*R_211fr*ionization(T,E_wf_Au,E_ip_Fr,0.5),t_211fr);
+	TGraph *g_211fr = new TGraph(N_211fr);
+	g_211fr->SetMarkerColor(kBlue);
+	g_211fr->SetLineColor(kBlue);
 
-	double br_211at = 0.4180;
-	double brerr_211at = 0.0008;
-	double t_211at = 7.214*hours;
-	double terr_211at = 0.007*hours;
+	double R_209fr = 0.262;
+	double br_209fr = 0.89;
+	double t_209fr = 50.0*seconds;
+	double e_209fr = 6.9; // MeV
 
-	double br_211po = 1.0;
-	double brerr_211po = 0.0;
-	double t_211po = 0.516*seconds;
-	double terr_211po = 0.003*seconds;
+	TF1 *N_209fr = new TF1("N_209Fr",parent,0.,timelimit,2);
+	N_209fr->SetParameters(secondaryFlux*R_209fr*ionization(T,E_wf_Au,E_ip_Fr,0.5),t_209fr);
+	TGraph *g_209fr = new TGraph(N_209fr);
+	g_209fr->SetMarkerColor(kOrange);
+	g_209fr->SetLineColor(kOrange);
 
-	double br_207at = 0.086;
-	double brerr_207at = 0.01;
-	double t_207at = 1.80*hours;
-	double terr_207at = 0.04*hours;
+	
+	TMultiGraph *N = new TMultiGraph("N","Number of Ions at MCP Surface; Time Elapsed (s); Ions");
+	N->Add(g_210fr);
+	N->Add(g_211fr);
+	N->Add(g_209fr);
 
-	double br_207po = 0.00021;
-	double brerr_207po = 0.00002;
-	double t_207po = 5.80*hours;
-	double terr_207po = 0.02*hours;
+	N->Draw("AL");
 
-	double br_207bi = 0.0;
-	double brerr_207bi = 0.0;
-	double t_207bi = 31.55*years;
-	double terr_207bi = 0.05*years;
+	c1->BuildLegend();
+	c1->Update();
+	c1->Modified();
 
-	double br_207pb = 0.0;
-	double brerr_207pb = 0.0;
-	double t_207pb = 10000.*years;
-	double terr_207pb = 0.0;
+	rootapp.Run();
+
+	return 0;
+
+//	double R_208rn = 0.001;
+//	double br_208rn = ;
+
+//	double R_211rn = 0.003;
+//	double br_211rn = 0.274;
+//	double t_211rn = 14.6*hours;
+//	double e_211rn = 6.0; // MeV
+
+//	double R_208at = 0.015;
+//	double br_208at;
+
+//	double R_207at = 0.049;
+//	double br_207at;
+	
+//	double R_206at = 0.006;
+
+
+//	double br_211rn = 0.274;
+//	double brerr_211rn = 0.017;
+//	double t_211rn = 14.6*hours;
+//	double terr_211rn = 0.2*hours;
+
+//	double br_211at = 0.4180;
+//	double brerr_211at = 0.0008;
+//	double t_211at = 7.214*hours;
+//	double terr_211at = 0.007*hours;
+
+//	double br_211po = 1.0;
+//	double brerr_211po = 0.0;
+//	double t_211po = 0.516*seconds;
+//	double terr_211po = 0.003*seconds;
+
+//	double br_207at = 0.086;
+//	double brerr_207at = 0.01;
+//	double t_207at = 1.80*hours;
+//	double terr_207at = 0.04*hours;
+
+//	double br_207po = 0.00021;
+//	double brerr_207po = 0.00002;
+//	double t_207po = 5.80*hours;
+//	double terr_207po = 0.02*hours;
+
+//	double br_207bi = 0.0;
+//	double brerr_207bi = 0.0;
+//	double t_207bi = 31.55*years;
+//	double terr_207bi = 0.05*years;
+
+//	double br_207pb = 0.0;
+//	double brerr_207pb = 0.0;
+//	double t_207pb = 10000.*years;
+//	double terr_207pb = 0.0;
+
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+decaychain:	decaychain.cpp
+	`root-config --cxx --cflags` -o decaychain decaychain.cpp `root-config --glibs`
